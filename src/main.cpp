cc0efd5d34cdeabfa67454f0720941338b8fd43d
niwantha33 <niwantha33@gmail.com>
2022-01-02 17:45:56 +0530

    Global vars.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
 
-void setup() {
+//golbal variables
+
+int counter = 10;
+
+float errorValue = 0.123456F;
+
+float result = 0.00F;
+
+void setup()
+{
   Serial.begin(9600);
-  
 }
 
-void loop() {
+void loop()
+{
+  counter++;
+
+  result = counter + errorValue;
+
+  Serial.print("Result :");
+
+  Serial.println(result);
   
+  delay(1000);
 }
